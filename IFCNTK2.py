3cbfe3583595debf5af25c901f56bc5c417d28d4
dzywaseda <74039761+dzywaseda@users.noreply.github.com>
2021-10-20 10:17:07 +0900

    Update IFCNTK2.py

--- IFCNTK2.py
+++ IFCNTK2.py
@@ -243,6 +243,7 @@ for it in range(sample_type):
 			tmp.append(index)
 		if x >= (samples*1000):
 			#tmp = sample(tmp, samples)
+			print(len(tmp))
 			tmp = tmp[:len(tmp)/20]
 			break
 	deadlist = deadlist + tmp		
