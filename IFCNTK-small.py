9c6b5ed83b4e8536b31210f6dac66feaf984402d
dzywaseda <74039761+dzywaseda@users.noreply.github.com>
2021-11-27 14:26:51 +0900

    Update IFCNTK-small.py

--- IFCNTK-small.py
+++ IFCNTK-small.py
@@ -253,7 +253,7 @@ for it in sample_type:
 			x = x + 1
 			tmp.append(index)
 		if x >= (samples*100):
-			tmp = tmp[75:80]
+			tmp = tmp[80:85]
 			#tmp = sample(tmp, 1)
 			break
 	deadlist = deadlist + tmp		
