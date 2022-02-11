e9fd2b970049a8c79f1e627ab5fc4c463b628572
dzywaseda <74039761+dzywaseda@users.noreply.github.com>
2022-02-11 23:11:34 +0900

    Update entroy.py

--- entroy.py
+++ entroy.py
@@ -267,8 +267,9 @@ def trains():
 			H[i][j] = xz(X[i], X[j], L[i], L[j], iL[i], iL[j])
 
 	print(H[0:6,0:6])
-	print(np.mean(H[2:, 0:1]))
-	print(np.mean(H[2:, 1:2]))
+	print("sum var and std")
+	print(np.mean(H[2:, 0:1]), np.var(H[2:, 0:1]), np.std(H[2:, 0:1]))
+	print(np.mean(H[2:, 1:2]), np.var(H[2:, 0:1]), np.std(H[2:, 0:1]))
 	#Solve kernel regression.
 	Y_train = np.ones((N_train, 100)) * -0.1
 	for i in range(N_train):
