d59ec2744b19532b0083b6b23500a26294558e88
dzywaseda <74039761+dzywaseda@users.noreply.github.com>
2022-02-13 01:06:21 +0900

    Update entroy.py

--- entroy.py
+++ entroy.py
@@ -270,7 +270,7 @@ def trains():
 	print("sum var and std")
 	h = 0
 	for i in range(100):
-		h = h + np.sqrt((H[2+i,0]  * H[2+i,0]) / H[0,0] * H[2+i, 2]))
+		h = h + np.sqrt((H[2+i,0]  * H[2+i,0]) / (H[0,0] * H[2+i, 2]))
 	print("average value exp1", h/100)
 
 	print(np.mean(H[2:102, 0:1]), np.var(H[2:, 0:1]), np.std(H[2:, 0:1]))
