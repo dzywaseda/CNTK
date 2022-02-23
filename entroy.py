3da3e9920e94d50d61b4b3836d0e5e65daf2e504
dzywaseda <74039761+dzywaseda@users.noreply.github.com>
2022-02-23 17:57:58 +0900

    Update entroy.py

--- entroy.py
+++ entroy.py
@@ -108,8 +108,8 @@ void trans(float s[32][32][32][32], float t[32][32][32][32], const float l[32][3
 	int x2 = threadIdx.x + ((blockIdx.z >> 2) << 3);
 	int y2 = threadIdx.y + ((blockIdx.z & 3) << 3);
 	float S = s[x1][y1][x2][y2], T = t[x1][y1][x2][y2], L = l[x1][y1], R = r[x2][y2], iL = il[x1][y1], iR = ir[x2][y2];
-        S = S * S * iL * iR;
-	float BS = (S * (3.141592654f - acosf(max(min(S, 1.0f), -1.0f))) + sqrtf(1.0f - min(S * S, 1.0f))) * L * R / S * 28.274333882308138f;
+        S = S * iL * iR;
+	float BS = (S * (3.141592654f - acosf(max(min(S, 1.0f), -1.0f))) + sqrtf(1.0f - min(S * S, 1.0f))) * L * R / 28.274333882308138f;
 	S = (3.141592654f - acosf(max(min(S, 1.0f), -1.0f))) / 28.274333882308138;
         t[x1][y1][x2][y2] = T * S + BS;
 	s[x1][y1][x2][y2] = BS;
@@ -148,6 +148,7 @@ def xx(x):
 	L = cp.sqrt(cp.diag(S.reshape(1024, 1024)).reshape(32, 32))
 	iL = 1.0 / L
 	RL.append(L)
+	print("RL", RL)
 	iRL.append(iL)
 	trans(trans_blocks, trans_threads, (S, T, L, L, iL, iL))	
 	
